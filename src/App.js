97da600765fb3f0965268cc983a0e612112bd1cc
konjoyeww <kemal.siraj@a2sv.org>
2023-10-30 07:13:04 +0300

    primary commponents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import Hero from './component/Hero';
+import About from './component/About';
+import Skills from './component/Skills';
+import Project from './component/Project';
+import Footer from './component/Footer';
+import Contact from './component/Contact';
+import Hireme from './component/Hireme';
+import Bot from './component/Bot';
+import Navbar from './component/Navbar';
+
 
 const App = () => {
+
+
   return (
     <div>
+      <Navbar />
+      <Bot />
+      <Hero />
+      <About />
+      <Skills />
+      <Hireme />
+      <Project />
+      <Contact />
+      <Footer />
+       
     </div>
-  )
-}
 
-export default App
+
+  );
+};
+
+export default App;
